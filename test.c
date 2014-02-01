e353aff08a680f512a6e45b3403aa3ba12e22991
lucastsa <lucas.sa@gmail.com>
2014-02-01 07:46:00 -0500

    adding tests for small obj allocation and perf comparison with malloc

--- test.c
+++ test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "slab.h"
 
+#define rdtscll(val) __asm__ __volatile__("rdtsc" : "=A" (val))
+
 /* test approach based inspired by MinUnit 
    http://www.jera.com/techinfo/jtns/jtn002.html */
 #define assertt(message, test) do { if (!(test)) return message; } while (0)
@@ -8,18 +10,95 @@
                                 if (message) return (char*)message; } while (0)
 int tests_run = 0;
 
-
 static char *
 test_cache_create() {
-    assertt("cache creation returned null?", 
-            kmem_cache_create("test", 8, 0, NULL, NULL));
+    kmem_cache_t cp = kmem_cache_create("test", 12, 0, NULL, NULL);
+    assertt("cache creation returned null?", cp);
+
+    assertt("effective size miscalculated", cp->effsize == 16);
+
+    kmem_cache_destroy(cp);
+
+    return 0;
+}
+
+static char *
+test_cache_grow() {
+    kmem_cache_t cp = kmem_cache_create("test", 12, 0, NULL, NULL);
+
+    kmem_cache_grow(cp);
+
+    kmem_cache_destroy(cp);
+
+    return 0;
+}
+
+static char *
+test_cache_alloc() {
+    // 12-byte struct
+    struct test {
+        int a, b, c;
+    };
+    struct test * obj;
+
+    kmem_cache_t cp = kmem_cache_create("test", sizeof(struct test), 0, NULL, NULL);
+
+    obj = (struct test *)kmem_cache_alloc(cp, KM_NOSLEEP);
+
+    obj->a=1;
+    obj->b=1;
+    obj->c=1;
+
+    obj = (struct test *)kmem_cache_alloc(cp, KM_NOSLEEP);
+
+    obj->a=1;
+    obj->b=1;
+    obj->c=1;
+
+    kmem_cache_destroy(cp);
+
+    return 0;
+}
+
+static char *
+test_perf_cache_alloc() {
+    #define ITERATIONS 30000000
+    unsigned long long start, end;    
+    int i;
+    // 12-byte struct
+    struct test {
+        int a, b, c;
+    };
+    struct test * obj;
+
+    kmem_cache_t cp = kmem_cache_create("test", sizeof(struct test), 0, NULL, NULL);
+
+    rdtscll(start);
+    for (i=0; i<ITERATIONS; i++)
+        obj = (struct test *)kmem_cache_alloc(cp, KM_NOSLEEP);
+    rdtscll(end);
+
+    printf("# %lld cycles for cache mem alloc\n", (end-start)/ITERATIONS);
+
+
+    rdtscll(start);
+    for (i=0; i<ITERATIONS; i++)
+        obj = (struct test *)malloc(sizeof(struct test));
+    rdtscll(end);
+
+    printf("# %lld cycles for malloc\n", (end-start)/ITERATIONS);
+
+    kmem_cache_destroy(cp);
 
     return 0;
 }
 
 static char *
 test_all () {
-    run_test(test_cache_create);
+    // run_test(test_cache_create);
+    // run_test(test_cache_grow);
+    // run_test(test_cache_alloc);
+    run_test(test_perf_cache_alloc);
 
     return 0;
 }
@@ -34,5 +113,6 @@ main(void) {
 
     printf("=====================\nTOTAL TESTS:\t%04d\n", tests_run);
 
+    printf("%d\n", (0 || 2014));
     return (result != 0);
 }
